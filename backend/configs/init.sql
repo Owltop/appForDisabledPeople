9d08365c957bae523d07c6d47ad1e178093cbab7
Andrew Roy <andrejroy777@gmail.com>
2024-05-12 16:41:31 +0300

    zhest

--- backend/configs/init.sql
+++ backend/configs/init.sql
@@ -1,17 +1,3 @@
 CREATE USER admin WITH ENCRYPTED PASSWORD 'admin1234';
 CREATE DATABASE main WITH OWNER admin;
 GRANT ALL PRIVILEGES ON DATABASE main TO admin;
--- CREATE TABLE users
--- (
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     username TEXT NOT NULL,
---     password INTEGER NOT NULL,
---     token TEXT,
---     first_name TEXT,
---     last_name TEXT,
---     date_of_birth TEXT,
---     email TEXT,
---     phone_number TEXT,
---     telgram TEXT
--- );
-
